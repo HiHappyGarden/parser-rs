60d18a57dceb77f9fd1a7551ea92fc040b2a52ba
Antonio Salsi <passy.linux@zresa.it>
2026-01-02 23:53:58 +0100

    fix: update version to 0.1.2 in Cargo.toml and enhance license information across multiple files

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,40 @@
+/***************************************************************************
+ *
+ * AT Command Parser
+ * Copyright (C) 2026 Antonio Salsi <passy.linux@zresa.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
 
 //! AT Command Parser Library
 //!
 //! This library provides a flexible parser for AT commands, commonly used in
 //! embedded systems and communication devices. It supports no_std environments.
 
-#![cfg_attr(feature = "enable_panic", no_std)]
+#![cfg_attr(any(feature = "enable_panic", feature = "osal_rs"), no_std)]
 
-#[cfg(feature = "enable_panic")]
+#[cfg(any(feature = "enable_panic", feature = "osal_rs"))]
 extern crate alloc;
 
 #[cfg(feature = "osal_rs")]
 extern crate osal_rs;
+
+#[cfg(any(feature = "enable_panic", feature = "osal_rs"))]
+use core::prelude::rust_2021::*;
+use core::iter::Iterator;
+use core::option::Option;
+use core::result::Result;
 
 #[cfg(feature = "enable_panic")]
 #[panic_handler]
